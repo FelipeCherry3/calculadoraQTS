201014f11bd14248dc2437998c77d5678416190b
FelipeCherry3 <felipeelias609@hotmail.com>
2024-03-24 18:01:07 -0300

    calc init

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Calculadora {
+
+    public int soma (int a, int b){
+        return a + b;
+    }
+    public int subtracao (int a, int b){
+        return a - b;
+    }
+    public int multi (int a, int b){
+        return a * b;
+    }
+    public float divisao (float a, float b){
+        return a / b;
+    }
+    public int divisao (int a, int b){
+        return a / b;
+    }
+}
