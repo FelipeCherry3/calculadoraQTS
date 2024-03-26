b4a65159e9df049f560dfe49ba82fc6beec6302f
FelipeCherry3 <felipeelias609@hotmail.com>
2024-03-26 15:05:46 -0300

    comments

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -1,26 +1,58 @@
 package org.example;
 
 /**
- * @author Felipe
+ * @author : Felipe
  * @version 1.0
  * @since release 1.0
  */
-
-
 public class Calculadora {
 
+    /**
+     *
+     * @param a primeiro valor para operação matemática
+     * @param b segundo valor para operação matemática
+     * @return resultado da operação matemática Soma
+     */
     public int soma (int a, int b){
         return a + b;
     }
+
+    /**
+     *
+     * @param a primeiro valor para operação matemática
+     * @param b segundo valor para operação matemática
+     * @return resultado da operação matemática Subtração
+     */
     public int subtracao (int a, int b){
         return a - b;
     }
+
+    /**
+     *
+     * @param a primeiro valor para operação matemática
+     * @param b segundo valor para operação matemática
+     * @return resultado da operação matemática Multiplicação
+     */
     public int multi (int a, int b){
         return a * b;
     }
+
+    /**
+     *
+     * @param a primeiro valor para operação matemática
+     * @param b segundo valor para operação matemática
+     * @return resultado da operação matemática Divisao
+     */
     public float divisao (float a, float b){
         return a / b;
     }
+
+    /**
+     *
+     * @param a primeiro valor para operação matemática
+     * @param b segundo valor para operação matemática
+     * @return resultado da operação matemática Divisao
+     */
     public int divisao (int a, int b){
         return a / b;
     }
